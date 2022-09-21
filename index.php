a881c362d49c56f436f19ab3e9fe320a32880906
Mark Price <mark@pricem.co.uk>
2022-09-21 23:11:15 +0100

    first conmit

---
+++ index.php
@@ -0,0 +1,21 @@
+
+<?php
+echo"<h1> test 1 </h1>";
+    echo "test";
+    echo '<br><br>';
+    $test = "this is a test";
+    $number = 2;
+    echo $test.' '.$number;
+
+echo"<h1> test 2 </h1>";
+echo '<br><br>';
+echo 'this is new text 2';
+echo '<br><br>';
+echo 'this is test 3';
+echo '<br><br>';
+echo 'this is another test';
+echo '<br><br>';
+
+
+
+?>
